8e6a2ec9fa709529ddde881bbf34c5f6d00f4d84
Vishnu Prakash J <82284517+vishnuprakash-777@users.noreply.github.com>
2021-04-22 00:41:30 +0530

    Add files via upload

---
+++ bigandsmall.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+  int a[50],n,i,min,max;
+  printf("Enter the number of values:");
+  scanf("%d",&n);
+  if(n>0)
+  {
+    printf("Enter the values:");
+    for(i=0;i<n;i++)
+    {
+      scanf("%d",&a[i]);
+    }
+    max=min=a[0];
+    for(i=0;i<n;i++)
+    {
+      if(a[i]>max)
+      {
+        max=a[i];
+      }
+      if(a[i]<min)
+      {
+        min=a[i];
+      }
+    }
+    printf("The largest value = %d \n",max);
+    printf("The smalllest value = %d \n",min);
+  }
+  else
+  {
+    printf("Invalid condition");
+  }
+}
