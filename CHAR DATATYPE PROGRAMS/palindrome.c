d58f9e22a133bc7aad0a4ff00a2813f755592a10
Vishnu Prakash J <82284517+vishnuprakash-777@users.noreply.github.com>
2021-05-04 17:41:28 +0530

    Add files via upload

---
+++ CHAR DATATYPE PROGRAMS/palindrome.c
@@ -0,0 +1,29 @@
+//Program to check wheather the word is palindrome or not.Also number of alphabets in the word.
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char a[20];
+  int count,i,found=0;
+  printf("Enter the word:");
+  scanf("%s",a);
+  count=strlen(a);
+  printf("No of alphabets = %d\n",count);
+  for(i=0;i<=count/2;i++)
+  {
+    if(a[i]!=a[count-(i+1)])
+    {
+      found=1;
+      break;
+    }
+  }
+  if(found==0)
+  {
+    printf("%s is a palindrome\n",a);
+  }
+  else
+  {
+    printf("%s is not a palindrome\n",a);
+  }
+
+}
