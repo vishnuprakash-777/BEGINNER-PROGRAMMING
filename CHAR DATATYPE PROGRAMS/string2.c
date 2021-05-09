a97ea53acc906aa858730525e72277789e06503e
Vishnu Prakash J <82284517+vishnuprakash-777@users.noreply.github.com>
2021-05-09 21:11:14 +0530

    Add files via upload

---
+++ CHAR DATATYPE PROGRAMS/string2.c
@@ -0,0 +1,19 @@
+//Given your username is "rahulk" & passward is "rak123". Write a program if the username and password are correct it prints "welcome" else it prints "access denied".
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char username[200], password[200];
+  printf("Enter the username:");
+  scanf("%s",username);
+  printf("Enter the password:");
+  scanf("%s",password);
+  if(strcmp(username,"rahulk")==0 && strcmp(password,"rak123")==0)
+  {
+    printf("Welcome\n");
+  }
+  else
+  {
+    printf("Access Denied\n");
+  }
+}
